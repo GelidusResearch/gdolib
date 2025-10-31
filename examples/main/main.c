40ac820da19ac0ffa12af52d110bb70a7ed48329
gelidusresearch <mike.laspina@shaw.ca>
2025-10-31 16:09:18 -0500

    Fix stop timer before delete + get_status on start

--- examples/main/main.c
+++ examples/main/main.c
@@ -1,7 +1,7 @@
 /* GdoLib - A library for controlling garage door openers.
  * Copyright (C) 2024  Konnected Inc.
  * Copyright (C) 2025  Gelidus Research Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
